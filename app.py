c6eb6e10547521faf18b215bdf94f7c4fe40c456
Bruno Di Franco Albuquerque <brunodifranco99@gmail.com>
2023-01-23 11:18:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,12 +20,12 @@ def predict():
     pipeline = Airbnb()
 
     # Loading credentials  
-    USER = os.environ.get('USER')
-    PASSWORD = os.environ.get('PASSWORD')
-    HOST = os.environ.get('HOST')
-    PORT = os.environ.get('PORT')
-    DATABASE = os.environ.get('DATABASE')
-
+    USER = os.getenv('USER')
+    PASSWORD = os.getenv('PASSWORD')
+    HOST = os.getenv('HOST')
+    PORT = os.getenv('PORT')
+    DATABASE = os.getenv('DATABASE')     
+    
     # Connect to Database
     conn_url = f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
 
